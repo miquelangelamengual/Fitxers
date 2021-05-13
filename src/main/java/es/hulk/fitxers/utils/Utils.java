0dd5e54889d866f3302973f3e87968fe3a6ff709
Miquel Angel <hulkrojo2@gmail.com>
2021-05-13 10:27:01 +0200

    Added simple utils

---
+++ src/main/java/es/hulk/fitxers/utils/Utils.java
@@ -0,0 +1,13 @@
+package es.hulk.fitxers.utils;
+
+public class Utils {
+
+    public static void newLine(String path) {
+        System.out.println(path);
+    }
+
+    public static void line(String path) {
+        System.out.println(path);
+    }
+
+}
