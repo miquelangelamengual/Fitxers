ef3997bfc5feb842e91b236797e8f8b0ffd52a7c
Miquel Angel <hulkrojo2@gmail.com>
2021-05-13 11:20:14 +0200

    Added 2 menus, one for login and other for register

--- src/main/java/es/hulk/fitxers/menu/Menu.java
+++ src/main/java/es/hulk/fitxers/menu/Menu.java
@@ -1,17 +1,36 @@
 package es.hulk.fitxers.menu;
 
 import es.hulk.fitxers.login.Login;
+import es.hulk.fitxers.login.Register;
 import es.hulk.fitxers.utils.Utils;
 
 import java.util.Scanner;
 
 public class Menu {
 
-    private static Scanner scanner = new Scanner(System.in);
-
     public static void firstMenu() {
+        Scanner scanner = new Scanner(System.in);
         Utils.newLine("");
-        Utils.newLine("Practica 3 - Fitxers d'access Aleatoris");
+        Utils.newLine("Practica 3 - Login");
+        Utils.newLine("");
+        Utils.newLine("1 - Login");
+        Utils.newLine("2 - Register");
+        Utils.newLine("");
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                loginMenu();
+                break;
+            case 2:
+                registerMenu();
+                break;
+        }
+    }
+
+    public static void loginMenu() {
+        Scanner scanner = new Scanner(System.in);
+        Utils.newLine("");
+        Utils.newLine("Practica 3 - Login");
         Utils.newLine("");
 
         Utils.line("Nom de usuari: ");
@@ -22,6 +41,26 @@ public class Menu {
 
         Login login = new Login(user, pass);
         login.validate();
+    }
+
+    public static void registerMenu() {
+        Scanner scanner = new Scanner(System.in);
+        Utils.newLine("");
+        Utils.newLine("Practica 3 - Register");
+        Utils.newLine("");
+
+        Utils.line("Tipo de usuari (admin o normal): ");
+        String type = scanner.nextLine();
+
+        Utils.line("Nom de Usuari: ");
+        String username = scanner.nextLine();
+
+        Utils.line("Contrasenya");
+        String password = scanner.nextLine();
+        Register reg = new Register();
+        reg.setPassword(password);
+        reg.setUsername(username);
+        reg.registerUser(type);
     }
 
 }
