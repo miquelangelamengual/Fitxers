fe7f164968dda0ffc721faf0cb4159ab8839d1d8
Miquel Angel <hulkrojo2@gmail.com>
2021-05-13 10:26:53 +0200

    Get TypeAccess for logged users

---
+++ src/main/java/es/hulk/fitxers/access/TypeAccess.java
@@ -0,0 +1,15 @@
+package es.hulk.fitxers.access;
+
+import es.hulk.fitxers.utils.Utils;
+
+public class TypeAccess {
+
+    public static void administrator() {
+        Utils.newLine("Admin");
+    }
+
+    public static void normalAccess() {
+        Utils.newLine("Normal");
+    }
+
+}
