b61e6cc0913ce43f8de8cb21f6d4a2bc0f8c8f09
Miquel Angel <hulkrojo2@gmail.com>
2021-05-18 13:01:08 +0200

    Divided perms in classes Administrator.java and Username.java

---
+++ src/main/java/es/hulk/fitxers/access/Administrator.java
@@ -0,0 +1,17 @@
+package es.hulk.fitxers.access;
+
+public class Administrator {
+
+    public static void add() {
+
+    }
+
+    public static void remove() {
+
+    }
+
+    public static void edit() {
+
+    }
+
+}
---
+++ src/main/java/es/hulk/fitxers/access/Username.java
@@ -0,0 +1,9 @@
+package es.hulk.fitxers.access;
+
+public class Username {
+
+    public static void read() {
+
+    }
+
+}
