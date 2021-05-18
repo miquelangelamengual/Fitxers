da2c1e01f5fe7b23233b9907b18eff0bdeaa6f92
Miquel Angel <hulkrojo2@gmail.com>
2021-05-18 13:00:35 +0200

    Removed useless code

--- src/main/java/es/hulk/fitxers/menu/Menu.java
+++ src/main/java/es/hulk/fitxers/menu/Menu.java
@@ -10,30 +10,6 @@ public class Menu {
 
     public static void firstMenu() {
         Scanner scanner = new Scanner(System.in);
-        Utils.newLine(Color.strikethrough + "--------------------------");
-        Utils.newLine("Practica 3 - Login");
-        Utils.newLine("");
-        Utils.newLine("1 - Login");
-        Utils.newLine("2 - Register");
-        Utils.newLine("");
-        Utils.newLine("3 - Surt del programa");
-        Utils.newLine(Color.strikethrough + "--------------------------");
-        int option = scanner.nextInt();
-        switch (option) {
-            case 1:
-                loginMenu();
-                break;
-            case 2:
-                registerMenu();
-                break;
-            case 3:
-                System.exit(404);
-                break;
-        }
-    }
-
-    public static void loginMenu() {
-        Scanner scanner = new Scanner(System.in);
         Utils.newLine("");
         Utils.newLine("Practica 3 - Login");
         Utils.newLine("");
@@ -46,21 +22,6 @@ public class Menu {
 
         Login login = new Login(user, pass);
         login.validate();
-    }
-
-    public static void registerMenu() {
-        Scanner scanner = new Scanner(System.in);
-        Utils.newLine("");
-        Utils.newLine("Practica 3 - Register");
-        Utils.newLine("");
-
-        Utils.line("Tipo de usuari (admin o normal): ");
-
-        Utils.line("Nom de Usuari: ");
-        String username = scanner.nextLine();
-
-        Utils.line("Contrasenya");
-        String password = scanner.nextLine();
     }
 
 }
