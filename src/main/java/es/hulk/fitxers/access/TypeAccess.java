5fd63a82e3a7c171ed92b2936e3d504f4ebe663e
Miquel Angel <hulkrojo2@gmail.com>
2021-05-18 13:00:27 +0200

    Added menu for admin and normal mode

--- src/main/java/es/hulk/fitxers/access/TypeAccess.java
+++ src/main/java/es/hulk/fitxers/access/TypeAccess.java
@@ -1,16 +1,64 @@
 package es.hulk.fitxers.access;
 
-import es.hulk.fitxers.utils.Color;
+import es.hulk.fitxers.menu.Menu;
 import es.hulk.fitxers.utils.Utils;
+
+import java.util.Scanner;
 
 public class TypeAccess {
 
+    private static int option;
+
     public static void adminAccess() {
-        Utils.newLine("");
+        Scanner scanner = new Scanner(System.in);
+        Utils.bar();
+        Utils.newLine(" Mode Administrador");
+        Utils.space();
+        Utils.newLine("1 - Inserir");
+        Utils.newLine("2 - Eliminar");
+        Utils.newLine("3 - Editar");
+        Utils.newLine("4 - Llegir");
+        Utils.space();
+        Utils.newLine("5 - Torna al menu principal");
+        Utils.bar();
+        option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                Administrator.add();
+                break;
+            case 2:
+                Administrator.remove();
+                break;
+            case 3:
+                Administrator.edit();
+                break;
+            case 4:
+                Username.read();
+                break;
+            case 5:
+                Menu.firstMenu();
+                break;
+        }
     }
 
     public static void normalAccess() {
-        Utils.newLine("Normal");
+        Scanner scanner = new Scanner(System.in);
+        Utils.bar();
+        Utils.newLine(" Mode Usuari ");
+        Utils.space();
+        Utils.newLine("1 - Llegir");
+        Utils.space();
+        Utils.newLine("2 - Tornar Enrere");
+        Utils.bar();
+        option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                Username.read();
+                break;
+            case 2:
+                Menu.firstMenu();
+                break;
+        }
     }
 
 }
