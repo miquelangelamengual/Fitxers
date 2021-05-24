08c5ce176a149e25e680fff4ac6adb0525ecaafb
Miquel Angel <hulkrojo2@gmail.com>
2021-05-24 09:14:02 +0200

    Updated Article.java

--- src/main/java/es/hulk/fitxers/types/Article.java
+++ src/main/java/es/hulk/fitxers/types/Article.java
@@ -4,76 +4,75 @@ public class Article {
 
     private String name;
     private String description;
-    private boolean active;
     private double price;
-    private int stock;
+    private Stock stock;
 
     private static int MAX_STRING_NAME = 20;
     private static int MAX_STRING_DESC = 120;
 
     public Article() {}
 
-    public String getName() {
-        return name;
-    }
+    /*
+     * SETTERS
+     */
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public boolean isActive() {
-        return active;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
-    public void setActive(boolean active) {
-        this.active = active;
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    /*
+     * GETTERS
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getStock() {
+    public Stock getStock() {
         return stock;
     }
 
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
     public String getAdjustedName() {
-        String adjustedName = getName();
+        StringBuilder adjustedName = new StringBuilder(getName());
         if (adjustedName.length() > MAX_STRING_NAME) {
-            adjustedName = adjustedName.substring(0, MAX_STRING_NAME);
-            return adjustedName;
+            adjustedName = new StringBuilder(adjustedName.substring(0, MAX_STRING_NAME));
+            return adjustedName.toString();
         }
         for (int i = getName().length();i < MAX_STRING_NAME; i++) {
-            adjustedName += " ";
+            adjustedName.append(" ");
         }
-        return adjustedName;
+        return adjustedName.toString();
     }
 
     public String getAdjustedDescription() {
-        String adjustedDescription = getDescription();
+        StringBuilder adjustedDescription = new StringBuilder(getDescription());
         if (adjustedDescription.length() > MAX_STRING_DESC) {
-            adjustedDescription = adjustedDescription.substring(0, MAX_STRING_DESC);
-            return adjustedDescription;
+            adjustedDescription = new StringBuilder(adjustedDescription.substring(0, MAX_STRING_DESC));
+            return adjustedDescription.toString();
         }
         for (int i = getName().length();i < MAX_STRING_DESC; i++) {
-            adjustedDescription += " ";
+            adjustedDescription.append(" ");
         }
-        return adjustedDescription;
+        return adjustedDescription.toString();
     }
 }
