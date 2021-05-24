e71f0d7669439706cd31f38d4b99aa9ff15daca6
Miquel Angel <hulkrojo2@gmail.com>
2021-05-24 09:13:40 +0200

    Maded some work with Stock.java

--- src/main/java/es/hulk/fitxers/types/Stock.java
+++ src/main/java/es/hulk/fitxers/types/Stock.java
@@ -2,6 +2,14 @@ package es.hulk.fitxers.types;
 
 public class Stock {
 
+    private boolean isActive;
+    private int quantity;
+
+    public Stock(boolean isActive, int quantity) {
+        this.isActive = isActive;
+        this.quantity = quantity;
+    }
+
 
 
 }
