ed03eecfce6b85b6a5b285955539848dff0e0080
Miquel Angel <hulkrojo2@gmail.com>
2021-05-24 09:52:10 +0200

    Updated main

--- src/main/java/es/hulk/fitxers/Fitxers.java
+++ src/main/java/es/hulk/fitxers/Fitxers.java
@@ -1,17 +1,23 @@
 package es.hulk.fitxers;
 
-import es.hulk.fitxers.accessfile.RandomAccessFile;
 import es.hulk.fitxers.menu.Menu;
-import es.hulk.fitxers.test.Student;
-import es.hulk.fitxers.test.StudentIO;
 import es.hulk.fitxers.types.Article;
-import es.hulk.fitxers.utils.Utils;
+import es.hulk.fitxers.types.StorageIO;
 
 import java.io.IOException;
 
 public class Fitxers {
 
     public static void main(String[] args) throws IOException {
+        Article article = new Article();
+        article.setActive(true);
+        article.setName("Mi Libro");
+        article.setDescription("Luna de plutionio");
+        article.setStock(20000);
+        article.setPrice(20);
+
+        StorageIO storage = new StorageIO("article.txt", "rw");
+        storage.add(article);
         Menu.firstMenu();
     }
 
