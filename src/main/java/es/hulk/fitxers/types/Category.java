df2bdc6448ca16ef9aac38792c2c59d794046cc4
Miquel Angel <hulkrojo2@gmail.com>
2021-05-24 16:11:10 +0200

    changed name to categories

--- src/main/java/es/hulk/fitxers/types/Category.java
+++ src/main/java/es/hulk/fitxers/types/Category.java
@@ -4,9 +4,9 @@ public enum Category {
 
     //TODO: rename category to make it have more sens
 
-    CATEGORIA1,
-    CATEGORIA2,
-    CATEGORIA3,
-    CATEGORIA4
+    SUCESOS,
+    INFORMATICA,
+    DEPORTES,
+    OCIO
 
 }
