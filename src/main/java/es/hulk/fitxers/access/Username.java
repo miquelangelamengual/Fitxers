b03544c0a0b33cefb732a11027126fc2b1598d6c
Miquel Angel <hulkrojo2@gmail.com>
2021-05-24 09:52:30 +0200

    added usermenu

--- src/main/java/es/hulk/fitxers/access/Username.java
+++ src/main/java/es/hulk/fitxers/access/Username.java
@@ -1,9 +1,30 @@
 package es.hulk.fitxers.access;
 
+import es.hulk.fitxers.menu.Menu;
+import es.hulk.fitxers.utils.Utils;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Username {
 
-    public static void read() {
-
+    public static void userMenu() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Utils.bar();
+        Utils.newLine(" Mode Usuari ");
+        Utils.space();
+        Utils.newLine("1 - Llegir");
+        Utils.space();
+        Utils.newLine("2 - Tornar Enrere");
+        Utils.bar();
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                Menu.firstMenu();
+                break;
+        }
     }
 
 }
