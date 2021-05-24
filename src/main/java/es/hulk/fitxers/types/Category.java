f547e2594ad3077c2f03dfa200afa8dc3fdb528d
Miquel Angel <hulkrojo2@gmail.com>
2021-05-24 11:51:26 +0200

    Added enum with 4 diferent categories

--- src/main/java/es/hulk/fitxers/types/Category.java
+++ src/main/java/es/hulk/fitxers/types/Category.java
@@ -2,6 +2,11 @@ package es.hulk.fitxers.types;
 
 public enum Category {
 
+    //TODO: rename category to make it have more sens
 
+    CATEGORIA1,
+    CATEGORIA2,
+    CATEGORIA3,
+    CATEGORIA4
 
 }
