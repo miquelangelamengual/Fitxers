bcf3379de80232d4bea05efcfd1d29d5fda9e8e5
Miquel Angel <hulkrojo2@gmail.com>
2021-05-24 09:52:03 +0200

    Added add method

--- src/main/java/es/hulk/fitxers/types/StorageIO.java
+++ src/main/java/es/hulk/fitxers/types/StorageIO.java
@@ -1,15 +1,23 @@
 package es.hulk.fitxers.types;
 
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class StorageIO {
 
     RandomAccessFile randomAccessFile;
 
-    public StorageIO() {
-
+    public StorageIO(String fileName, String mode) throws IOException {
+        randomAccessFile = new RandomAccessFile(fileName, mode);
     }
 
-
+    public void add(Article article) throws IOException {
+        randomAccessFile.seek(randomAccessFile.length());
+        randomAccessFile.writeBoolean(article.isActive());
+        randomAccessFile.writeChars(article.getAdjustedName());
+        randomAccessFile.writeChars(article.getAdjustedDescription());
+        randomAccessFile.writeDouble(article.getPrice());
+        randomAccessFile.writeInt(article.getStock());
+    }
 
 }
